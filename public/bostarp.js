57594788cc1177b88cf6d686cd17893f513e784c
binnypatel <binnypatel14@gmail.com>
2015-05-12 14:03:07 -0400

    page

--- public/bostarp.js
+++ public/bostarp.js
@@ -1,17 +1,31 @@
 $(document).ready(function(){
-  
-  $.ajax({
-    url:"/products",
-    type:"get",
-    success:function(mydata){
-      for(var i=0;i<mydata.length;i++){
-        $(".page").append('<div class="con container well col-md-8 pull-left"><span class="glyphicon glyphicon-remove-sign pull-right"id='+mydata[i].id+'></span><button class="btn btn-primary btn-sm pull-right">'+mydata[i].price+'</button><h3 class="item">'+mydata[i].item+'</h3><h4 class="des pull-left">'+mydata[i].des+'</h4></div>');
-      }
-    }
-  })
+  console.log("hi")    
+ $(".page").on("click","span",function(){
+   console.log("hello");
+    $(this).parent('div').remove();
+        console.log(this.id)
+        $.ajax({
+            
+                type : "delete",
+                url : "/products/" + this.id,
+                    
+                success : function(data){    
+                    //$(this).parent('div').remove();         
+                }
+        });
+    });
+        $.ajax({
+              url:"/products/",
+              type:"get",
+              success:function(mydata){
+                 for(i=0;i<mydata.length;i++){
+                 $(".page").append('<div class="con container well col-md-8 pull-left"><span id="'+mydata[i].id+'" class="delete glyphicon glyphicon-remove pull-right"></span><button class="btn btn-primary btn-sm pull-right">'+mydata[i].price+'</button><h3 class="item">'+mydata[i].item+'</h3><h4 class="des pull-left">'+mydata[i].des+'</h4></div>')
+                }    
+              }
+        });
 
-        $(".btn").on('click',function(event){
-          console.log(this);
+    $(".btn").on('click',function(event){
+          //console.log(this);
          event.preventDefault();
           var item =$("input[name='item']").val();
           var des =$("input[name='des']").val();
@@ -27,12 +41,10 @@ $(document).ready(function(){
             dataType:'json',
 
             success:function(data){
-              console.log(data);
+             // console.log(data);
               $(".page").append('<div class="con container well col-md-8 pull-left"><span class="glyphicon glyphicon-remove-sign pull-right"id='+data.id+'></span><button class="btn btn-primary btn-sm pull-right">'+data.price+'</button><h3 class="item">'+data.item+'</h3><h4 class="des pull-left">'+data.des+'</h4></div>')
                 
             }
             });
           });
         });
-
-      
